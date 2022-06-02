85011452cbc756ad4e205d2e5fcdfde0b0cdaf9d
roodesh <36762190+roodesh@users.noreply.github.com>
2022-06-02 09:48:44 +0530

    update sales_order.py

--- stonewarehouse/stonewarehouse/dashboard/sales_order.py
+++ stonewarehouse/stonewarehouse/dashboard/sales_order.py
@@ -4,27 +4,26 @@ from frappe import _
 def get_data(data):
 	data['non_standard_fieldnames'] = {
 		'Delivery Note': 'against_sales_order',
-		'Auto Repeat': 'reference_document',
-		'Pick List': 'sales_order'
+		'Pick List': 'sales_order',
+		"Journal Entry": "reference_name",
+		"Payment Entry": "reference_name",
+		"Payment Request": "reference_name",
+		"Auto Repeat": "reference_document",
+		"Maintenance Visit": "prevdoc_docname",
 	}
 
 	data['transactions'] = [
 		{
 			'label': _('Fulfillment'),
-			'items': ['Pick List', 'Delivery Note']
+			"items": ["Sales Invoice", "Pick List", "Delivery Note", "Maintenance Visit"],
 		},
-		{
-			'label': _('Purchasing'),
-			'items': ['Material Request']
-		},
+		{"label": _("Purchasing"), "items": ["Material Request", "Purchase Order"]},
 		{
 			'label': _('Manufacturing'),
 			'items': ['Work Order']
 		},
-		{
-			'label': _('Reference'),
-			'items': ['Auto Repeat']
-		}
+		{"label": _("Reference"), "items": ["Quotation", "Auto Repeat"]},
+		{"label": _("Payment"), "items": ["Payment Entry", "Payment Request", "Journal Entry"]},
 	]
 
 	return data
